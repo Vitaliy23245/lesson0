29512bd7df881562ed895ada49b40d6ce64bfd8a
Vitaliy23245 <Vitakup202@mail.ru>
2024-09-02 19:09:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print(9**0.5*5) # 1st program
-print(9.99>99.8 or 1000<=1000.1)# 2nd program
+print(not 9.99>99.8 and 1000<=1000.1)# 2nd program
 print(2*2+2==2*(2+2))#3nd program
+print(int(float('123.456')*10)%10)
